80624fa398e4796cafb218a709bff5d3478ad7c3
Nick Inman <nick@theoatmeal.com>
2019-02-19 17:32:30 +0000

    Automatically backed up by Learn

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,15 @@
+class SessionController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    session[:name] = params[:name]
+    redirect_to '/'
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
